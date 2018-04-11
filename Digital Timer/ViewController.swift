d343609655631a6ec9e6a65baa484c84191cabfb
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-11 11:40:41 +0900

    timer

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var myTimer = Timer()
+    @IBOutlet weak var mylbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
+            #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE"
+        mylbl.text = formatter.string(from: date)
     }
 
     override func didReceiveMemoryWarning() {
