8d1909f89e9b64ce1916e259ffea6a825cddb8f8
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-11 12:18:17 +0900

    closure

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-var myTimer = Timer()
+    
+    var myTimer = Timer()
+    var chack = false
+    @IBOutlet weak var aSS: UISwitch!
     @IBOutlet weak var mylbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
-            #selector(updateTime), userInfo: nil, repeats: true)
+        aSS.setOn(false, animated: true)
+        
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
+//            #selector(updateTime), userInfo: nil, repeats: true)
+        //클로져(closure)의 사용
+//        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (myTimer) in
+//            self.updateTime()
+//            })
     }
     
     @objc func updateTime() {
@@ -24,7 +34,17 @@ var myTimer = Timer()
         formatter.dateFormat = "HH:mm:ss EE"
         mylbl.text = formatter.string(from: date)
     }
-
+    @IBAction func aS(_ sender: Any) {
+        if aSS.isOn == true {
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (myTimer) in
+                self.updateTime()
+                })
+        } else {
+            myTimer.invalidate()
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
